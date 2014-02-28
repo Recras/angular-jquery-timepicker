8a409f3c68087569d848b99ea546dc614e466360
Tijmen Klein <timmipetit@gmail.com>
2014-02-28 10:14:12 +0100

    Make the testing actually work.

--- test/test.conf.js
+++ test/test.conf.js
@@ -1,11 +1,10 @@
  module.exports = function(config) {
     config.set({
+        frameworks: ['jasmine'],
         basePath : '..',
         files : [
-            JASMINE,
-            JASMINE_ADAPTER,
-            'bower_components/jquery/jquery.js',
-            'bower_components/angular/angular.js',
+            'bower_components/jquery/dist/jquery.min.js',
+            'bower_components/angular/angular.min.js',
             'bower_components/angular-mocks/angular-mocks.js',
             'bower_components/jquery-timepicker-jt/jquery.timepicker.min.js',
             'src/timepickerdirective.js',
--- test/timepicker.spec.js
+++ test/timepicker.spec.js
@@ -1,6 +1,6 @@
 
 /*global describe, beforeEach, afterEach, it, inject, expect, module, dump, $*/
-describe('uiDate', function() {
+describe('uiTimepicker', function() {
     'use strict';
     beforeEach(module('ui.timepicker'));
     describe('simple use on span element', function() {
@@ -8,8 +8,21 @@ describe('uiDate', function() {
             inject(function($compile, $rootScope) {
                 var element;
                 element = $compile("<input ui-timepicker/>")($rootScope);
-                expect(element.timepicker()).toBeDefined();
+                expect(element.timepicker).toBeDefined();
             });
         });
+
+        it('should be able to get the date from the model', function() {
+            inject(function($compile, $rootScope) {
+                var element, aDate;
+                aDate = new Date(2010, 12, 1, 14, 15);
+                element = $compile("<input ui-timepicker ng-model='x'/>")($rootScope);
+                $rootScope.$apply(function() {
+                    $rootScope.x = aDate;
+                });
+                expect(element.timepicker('getTime', aDate)).toEqual(aDate);
+            });
+        });
+
     });
 });
