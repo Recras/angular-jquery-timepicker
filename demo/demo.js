3fbc32c65efd6e11391ed3bc2101e4e81222f392
Tijmen Klein <timmipetit@gmail.com>
2014-04-02 17:47:55 +0200

    Fix the directive to work on input elements
    We're using the $parsers to fetch the date object from the timepicker

---
+++ demo/demo.js
@@ -0,0 +1,13 @@
+var app = angular.module('testApp', ['ui.timepicker']);
+
+app.controller('TimepickerCtrl', function ($scope) {
+    $scope.date = new Date('2014-01-01T18:00:00');
+
+    $scope.increaseDate = function(date) {
+        date.setDate(date.getDate() + 1);
+    };
+
+    $scope.increaseHour = function(date) {
+        date.setHours(date.getHours() + 1);
+    };
+});
