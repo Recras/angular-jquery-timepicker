745b5a9e914c53c9d6c6644d68b531831a427afb
Tijmen Klein <timmipetit@gmail.com>
2014-04-03 12:01:09 +0200

    Fix the tests

--- test/timepicker.spec.js
+++ test/timepicker.spec.js
@@ -4,11 +4,9 @@ describe('uiTimepicker', function() {
     'use strict';
     beforeEach(module('ui.timepicker'));
     describe('simple use on span element', function() {
-        it('should have a timepicker attached', function() {
+        it('should not work without ngModel', function() {
             inject(function($compile, $rootScope) {
-                var element;
-                element = $compile("<span ui-timepicker/>")($rootScope);
-                expect(element.timepicker).toBeDefined();
+                expect(function() { $compile("<span ui-timepicker/>")($rootScope); }).toThrow();
             });
         });
 
@@ -29,6 +27,39 @@ describe('uiTimepicker', function() {
                 var element, aDate;
                 aDate = new Date(2010, 12, 1, 14, 15);
                 element = $compile("<span ui-timepicker ng-model='x'/>")($rootScope);
+                $rootScope.$apply();
+                element.timepicker('setTime', aDate);
+                expect($rootScope.x.hour).toEqual(aDate.hour);
+                expect($rootScope.x.minute).toEqual(aDate.minute);
+            });
+        });
+
+    });
+
+    describe('simple use on input element', function() {
+        it('should not work without ngModel', function() {
+            inject(function($compile, $rootScope) {
+                expect(function() { $compile("<input ui-timepicker/>")($rootScope); }).toThrow();
+            });
+        });
+
+        it('should be able to get the date from the model', function() {
+            inject(function($compile, $rootScope) {
+                var element, aDate;
+                aDate = new Date(2010, 12, 1, 14, 15);
+                element = $compile("<input ui-timepicker ng-model='x'/>")($rootScope);
+                $rootScope.$apply(function() {
+                    $rootScope.x = aDate;
+                });
+                expect(element.timepicker('getTime', aDate)).toEqual(aDate);
+            });
+        });
+
+        it('should put the date in the model', function() {
+            inject(function($compile, $rootScope) {
+                var element, aDate;
+                aDate = new Date(2010, 12, 1, 14, 15);
+                element = $compile("<input ui-timepicker ng-model='x'/>")($rootScope);
                 $rootScope.$apply();
                 element.timepicker('setTime', aDate);
                 expect($rootScope.x.hour).toEqual(aDate.hour);
