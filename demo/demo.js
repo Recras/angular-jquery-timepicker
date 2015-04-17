901a8363b43799b8c739403059fd36f2fcda7570
timmipetit <timmipetit@gmail.com>
2015-04-17 13:34:14 +0200

    Update the demo

--- demo/demo.js
+++ demo/demo.js
@@ -2,6 +2,10 @@ var app = angular.module('testApp', ['ui.timepicker']);
 
 app.controller('TimepickerCtrl', function ($scope) {
     $scope.date = new Date('2014-01-01T18:00:00');
+    $scope.options = {
+        step: 5,
+        timeFormat: 'H:i'
+    };
 
     $scope.increaseDate = function(date) {
         date.setDate(date.getDate() + 1);
